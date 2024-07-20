85dffe783d44ecaff0c0d23826abc8cf797c8b84
sseul <hanbat107@naver.com>
2024-07-20 23:53:27 +0900

    update : 로그인/회원가입 페이지 테스트 add : middleware 파일 추가

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,83 @@
+// app/auth/login/page.tsx
+
+"use client";
+
+import { supabase } from "@/utils/supabase/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+
+export default function LoginPage() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const router = useRouter();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!email || !password) {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    router.replace("/");
+  };
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col gap-y-2 border-2 w-96 items-center"
+    >
+      <div className="w-full flex items-center justify-between p-4">
+        <label htmlFor="email" className="mr-2">
+          이메일
+        </label>
+        <input
+          ref={emailRef}
+          type="email"
+          id="email"
+          placeholder="이메일을 입력하세요."
+          className="border-2 border-gray-300 rounded-md p-1"
+        />
+      </div>
+      <div className="w-full flex items-center justify-between p-4">
+        <label htmlFor="password" className="mr-2">
+          비밀번호
+        </label>
+        <input
+          ref={passwordRef}
+          type="password"
+          id="password"
+          placeholder="비밀번호를 입력하세요."
+          className="border-2 border-gray-300 rounded-md p-1"
+        />
+      </div>
+      <div className="w-full p-4 flex flex-col gap-y-2">
+        <button className="w-full bg-blue-500 text-white p-2 rounded-md">
+          로그인
+        </button>
+        <Link href="/auth/signup">
+          <button
+            type="button"
+            className="w-full bg-black text-white p-2 rounded-md"
+          >
+            회원가입 가기
+          </button>
+        </Link>
+      </div>
+    </form>
+  );
+}
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { supabase } from "@/utils/supabase/client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [error, setError] = useState({
+    password: "",
+    passwordConfirm: "",
+  });
+
+  const router = useRouter();
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 6) {
+      setError({
+        ...error,
+        password: "비밀번호는 최소 6자 이상입니다.",
+      });
+    } else {
+      setError({
+        ...error,
+        password: "",
+      });
+    }
+  };
+
+  const onChangePasswordConfirm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirm(e.target.value);
+    if (e.target.value.length < 6) {
+      setError({
+        ...error,
+        passwordConfirm: "비밀번호 확인은 최소 6자 이상입니다.",
+      });
+    } else {
+      setError({
+        ...error,
+        passwordConfirm: "",
+      });
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (email === "" || password === "" || passwordConfirm === "") {
+      alert("모든 항목을 입력해주세요.");
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      return alert(error.message);
+    }
+    alert("회원가입이 완료되었습니다.");
+    // router.push("/")
+    router.replace("/"); // 뒤로가기 시 회원가입 페이지로 이동 X
+  };
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col gap-y-2 border-2 w-96 items-center"
+    >
+      <div className="w-full flex items-center justify-between p-4">
+        <label htmlFor="email" className="mr-2">
+          이메일
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={onChangeEmail}
+          placeholder="이메일을 입력하세요."
+          className="border-2 border-gray-300 rounded-md p-1"
+        />
+      </div>
+      <div className="w-full flex items-center justify-between p-4">
+        <label htmlFor="password" className="mr-2">
+          비밀번호
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={onChangePassword}
+          placeholder="비밀번호를 입력하세요."
+          className="border-2 border-gray-300 rounded-md p-1"
+        />
+      </div>
+      {error.password && <p className="text-red-500">{error.password}</p>}
+      <div className="w-full flex items-center justify-between p-4">
+        <label htmlFor="passwordConfirm" className="mr-2">
+          비밀번호 확인
+        </label>
+        <input
+          type="password"
+          id="passwordConfirm"
+          value={passwordConfirm}
+          onChange={onChangePasswordConfirm}
+          placeholder="이메일을 입력하세요."
+          className="border-2 border-gray-300 rounded-md p-1"
+        />
+      </div>
+      {error.passwordConfirm && (
+        <p className="text-red-500">{error.passwordConfirm}</p>
+      )}
+      <div className="w-full p-4 flex flex-col gap-y-2">
+        <button className="w-full bg-blue-500 text-white p-2 rounded-md">
+          회원가입
+        </button>
+        <Link href="/auth/login">
+          <button
+            type="button"
+            className="w-full bg-black text-white p-2 rounded-md"
+          >
+            로그인하러 가기
+          </button>
+        </Link>
+      </div>
+    </form>
+  );
+}
