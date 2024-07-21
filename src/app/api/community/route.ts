f53a3a034eddbea551ee52b0f0248335b11cdb84
ydmaad <ydmaad@gmail.com>
2024-07-21 21:19:44 +0900

    update: 커뮤니티 route.ts GET,POST 생성

---
+++ src/app/api/community/route.ts
@@ -0,0 +1,65 @@
+import { supabase } from "@/utils/supabase/client";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Post {
+  avatar: string;
+  contents: string;
+  created_at: string;
+  id: string;
+  img_url: string;
+  nickname: string;
+  title: string;
+  user_id: string;
+}
+
+export async function GET() {
+  try {
+    const { data, error } = await supabase.from("posts").select("*");
+
+    if (error) {
+      return NextResponse.json(
+        { error: "데이터 조회 실패", message: error.message },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json({ message: "조회 성공", data }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error", message: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  const body: Post = await request.json();
+
+  try {
+    const { data, error } = await supabase.from("posts").insert(body).select();
+
+    if (error) {
+      return NextResponse.json(
+        { error: "등록실패", message: error.message },
+        { status: 500 }
+      );
+    }
+    return NextResponse.json({ message: "등록성공", data }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error", message: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+// export async function DELETE(request : Request) {
+//     const {id} = await request.json
+//   const { data, error } = await supabase.from("posts").delete().eq("id", id);
+
+//   if (error) {
+//     return NextResponse.json(error.message);
+//   }
+//   return NextResponse.json(data);
+// }
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,207 +4,207 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       calendar: {
         Row: {
-          created_at: string
-          id: string
-          medi_name: string
-          medi_time: string
-          side_effect: string
-          user_id: string
-        }
+          created_at: string;
+          id: string;
+          medi_name: string;
+          medi_time: string;
+          side_effect: string;
+          user_id: string;
+        };
         Insert: {
-          created_at?: string
-          id?: string
-          medi_name: string
-          medi_time: string
-          side_effect: string
-          user_id?: string
-        }
+          created_at?: string;
+          id?: string;
+          medi_name: string;
+          medi_time: string;
+          side_effect: string;
+          user_id?: string;
+        };
         Update: {
-          created_at?: string
-          id?: string
-          medi_name?: string
-          medi_time?: string
-          side_effect?: string
-          user_id?: string
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          id?: string;
+          medi_name?: string;
+          medi_time?: string;
+          side_effect?: string;
+          user_id?: string;
+        };
+        Relationships: [];
+      };
       column: {
         Row: {
-          descriptions: string
-          id: string
-          imgs_url: string
-          subtitle: string
-          title: string
-        }
+          descriptions: string;
+          id: string;
+          imgs_url: string;
+          subtitle: string;
+          title: string;
+        };
         Insert: {
-          descriptions: string
-          id?: string
-          imgs_url: string
-          subtitle: string
-          title: string
-        }
+          descriptions: string;
+          id?: string;
+          imgs_url: string;
+          subtitle: string;
+          title: string;
+        };
         Update: {
-          descriptions?: string
-          id?: string
-          imgs_url?: string
-          subtitle?: string
-          title?: string
-        }
-        Relationships: []
-      }
+          descriptions?: string;
+          id?: string;
+          imgs_url?: string;
+          subtitle?: string;
+          title?: string;
+        };
+        Relationships: [];
+      };
       medicine: {
         Row: {
-          id: string
-          medi_description: string
-          medi_name: string
-        }
+          id: string;
+          medi_description: string;
+          medi_name: string;
+        };
         Insert: {
-          id?: string
-          medi_description: string
-          medi_name: string
-        }
+          id?: string;
+          medi_description: string;
+          medi_name: string;
+        };
         Update: {
-          id?: string
-          medi_description?: string
-          medi_name?: string
-        }
-        Relationships: []
-      }
+          id?: string;
+          medi_description?: string;
+          medi_name?: string;
+        };
+        Relationships: [];
+      };
       pill_alarm: {
         Row: {
-          alarm_description: string
-          alarm_name: string
-          alarm_time: string
-          created_at: string
-          id: string
-          medi_name: string
-          user_id: string
-        }
+          alarm_description: string;
+          alarm_name: string;
+          alarm_time: string;
+          created_at: string;
+          id: string;
+          medi_name: string;
+          user_id: string;
+        };
         Insert: {
-          alarm_description: string
-          alarm_name: string
-          alarm_time: string
-          created_at?: string
-          id?: string
-          medi_name: string
-          user_id?: string
-        }
+          alarm_description: string;
+          alarm_name: string;
+          alarm_time: string;
+          created_at?: string;
+          id?: string;
+          medi_name: string;
+          user_id?: string;
+        };
         Update: {
-          alarm_description?: string
-          alarm_name?: string
-          alarm_time?: string
-          created_at?: string
-          id?: string
-          medi_name?: string
-          user_id?: string
-        }
-        Relationships: []
-      }
+          alarm_description?: string;
+          alarm_name?: string;
+          alarm_time?: string;
+          created_at?: string;
+          id?: string;
+          medi_name?: string;
+          user_id?: string;
+        };
+        Relationships: [];
+      };
       posts: {
         Row: {
-          avatar: string
-          contents: string
-          created_at: string
-          id: string
-          img_url: string
-          nickname: string
-          title: string
-          user_id: string
-        }
+          avatar: string;
+          contents: string;
+          created_at: string;
+          id: string;
+          img_url: string;
+          nickname: string;
+          title: string;
+          user_id: string;
+        };
         Insert: {
-          avatar: string
-          contents: string
-          created_at?: string
-          id?: string
-          img_url: string
-          nickname: string
-          title: string
-          user_id?: string
-        }
+          avatar: string;
+          contents: string;
+          created_at?: string;
+          id?: string;
+          img_url: string;
+          nickname: string;
+          title: string;
+          user_id?: string;
+        };
         Update: {
-          avatar?: string
-          contents?: string
-          created_at?: string
-          id?: string
-          img_url?: string
-          nickname?: string
-          title?: string
-          user_id?: string
-        }
+          avatar?: string;
+          contents?: string;
+          created_at?: string;
+          id?: string;
+          img_url?: string;
+          nickname?: string;
+          title?: string;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "posts_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: "posts_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
       user_medicine: {
         Row: {
-          medicine_id: string
-          user_id: string
-        }
+          medicine_id: string;
+          user_id: string;
+        };
         Insert: {
-          medicine_id?: string
-          user_id?: string
-        }
+          medicine_id?: string;
+          user_id?: string;
+        };
         Update: {
-          medicine_id?: string
-          user_id?: string
-        }
-        Relationships: []
-      }
+          medicine_id?: string;
+          user_id?: string;
+        };
+        Relationships: [];
+      };
       users: {
         Row: {
-          avatar: string
-          created_at: string
-          email: string
-          id: string
-          nickname: string
-        }
+          avatar: string;
+          created_at: string;
+          email: string;
+          id: string;
+          nickname: string;
+        };
         Insert: {
-          avatar: string
-          created_at?: string
-          email: string
-          id?: string
-          nickname: string
-        }
+          avatar: string;
+          created_at?: string;
+          email: string;
+          id?: string;
+          nickname: string;
+        };
         Update: {
-          avatar?: string
-          created_at?: string
-          email?: string
-          id?: string
-          nickname?: string
-        }
-        Relationships: []
-      }
-    }
+          avatar?: string;
+          created_at?: string;
+          email?: string;
+          id?: string;
+          nickname?: string;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -213,23 +213,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+      PublicSchema["Views"])
+  ? (PublicSchema["Tables"] &
+      PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -237,20 +237,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -258,20 +258,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -279,9 +279,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+  : never;
