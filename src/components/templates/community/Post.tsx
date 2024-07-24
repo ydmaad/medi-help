081859dab2bc1bc2d535db8c512917799675156c
ydmaad <ydmaad@gmail.com>
2024-07-24 20:12:44 +0900

    update:globals.css 코드 삭제

--- src/components/templates/community/Post.tsx
+++ src/components/templates/community/Post.tsx
@@ -9,36 +9,71 @@ const fetchPost = async ({
   title: string;
   contents: string;
 }) => {
-  const reponse = await fetch(`/api/community/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title: title, contents: contents }),
-  });
-  const data = await reponse.json();
-  return data;
+  try {
+    const response = await fetch(`/api/community/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title: title, contents: contents }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`서버 오류: ${response.status} ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("게시글 등록 오류 =>", error);
+  }
 };
 
 const Post = () => {
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
+  const [title, setTitle] = useState<string>("");
+  const [contents, setContents] = useState<string>("");
 
   return (
     <>
-      <div>글쓰기 하는데야!!!</div>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <input
-        type="text"
-        value={contents}
-        onChange={(e) => setContents(e.target.value)}
-        width={500}
-        height={500}
-      />
+      <div className="w-[700px] mx-auto p-4 bg-white shadow-md rounded-lg">
+        <h1 className="text-2xl font-bold mb-4">글쓰기 하는데야!!!</h1>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-semibold mb-2"
+            htmlFor="title"
+          >
+            제목
+          </label>
+          <input
+            type="text"
+            placeholder="제목을 입력하세요"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-semibold mb-2"
+            htmlFor="contents"
+          >
+            내용
+          </label>
+          <textarea
+            placeholder="내용을 입력하세요"
+            value={contents}
+            onChange={(e) => setContents(e.target.value)}
+            className="w-full h-[500px] px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="flex space-x-4">
+          <button className="bg-gray-300  px-4 py-2 rounded-md shadow-sm hover:bg-gray-400">
+            취소
+          </button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600">
+            게시하기
+          </button>
+        </div>
+      </div>
     </>
   );
 };
