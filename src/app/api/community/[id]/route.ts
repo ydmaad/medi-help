45b40ce3a8ea734ad344399c9442999966ff2a9b
ydmaad <ydmaad@gmail.com>
2024-07-25 10:30:36 +0900

    update: 게시글 삭제 구현중

--- src/app/api/community/[id]/route.ts
+++ src/app/api/community/[id]/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
-import React from "react";
 
 const GET = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const response = await fetch(`http://localhost:3000/community/${id}`);
   const data = response.json();
-  console.log(data);
   return NextResponse.json(data);
 };
 
