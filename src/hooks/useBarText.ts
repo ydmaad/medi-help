610450a031e078a97697dd6cb19dd9c83d5e8f23
lsy8181 <a4630486@gmail.com>
2024-07-28 19:27:12 +0900

    "update: 에러 메시지 수정"

--- src/hooks/useBarText.ts
+++ src/hooks/useBarText.ts
@@ -1,10 +1,10 @@
 import { useContext } from "react";
-import BarTextContext from "@/contexts/BarTextContext"; // 수정된 경로
+import BarTextContext from "@/contexts/BarTextContext";
 
 export const useBarText = () => {
-  const context = useContext(BarTextContext); // 수정된 부분
+  const context = useContext(BarTextContext);
   if (!context) {
-    throw new Error("테스트 에러 메시지 입니다.");
+    throw new Error("BarText 에러 메시지 입니다.");
   }
   return context;
 };
--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -4,7 +4,7 @@ import ImageContext from "@/contexts/ImageContext";
 export const useImage = () => {
   const context = useContext(ImageContext);
   if (!context) {
-    throw new Error("테스트 에러 메시지 입니다.");
+    throw new Error("Image 에러 메시지 입니다.");
   }
   return context;
 };
