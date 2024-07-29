20663562d938d138a58bc0108e9b4d17d5569b86
lsy8181 <a4630486@gmail.com>
2024-07-29 15:11:52 +0900

    add : 타이틀 내용작성

--- src/components/atoms/Title.tsx
+++ src/components/atoms/Title.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import useTitle from "@/hooks/useTitle";
 
-const Title: React.FC = () => {
-  const { title } = useTitle();
+interface TitleProps {
+  children: React.ReactNode;
+}
 
+const Title = ({ children }: TitleProps) => {
   return (
     <h1 className="text-left font-bold text-[32px] text-gray-900 mb-10 mt-20">
-      {title || "제목이 설정되지 않았습니다."}
+      {children}
     </h1>
   );
 };
