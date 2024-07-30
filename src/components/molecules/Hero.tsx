7dee570cab649240ce3a25daa3589e9517f5fe41
lsy8181 <a4630486@gmail.com>
2024-07-30 16:38:01 +0900

    style : 메인 검색창 구현

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import BgLinear from "@/components/atoms/BgLinear";
 import MainLogo from "@/components/atoms/MainLogo";
+import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
-    <div className="relative mb-[79px]">
-      <BgLinear />
-      <div className="absolute inset-0 flex justify-center items-center">
-        <MainLogo />
+    <div className="relative flex flex-col justify-center items-center">
+      <div className="absolute inset-0 z-0">
+        <BgLinear />
+      </div>
+      <div className="mt-[206px] mb-[349px] z-10">
+        <div className="mb-6 z-10">
+          <MainLogo />
+        </div>
+        <SearchBar />
       </div>
     </div>
   );
