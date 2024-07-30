efe3e110dec7c506b5fc4cf5dc2a26b592295830
lsy8181 <a4630486@gmail.com>
2024-07-30 17:19:49 +0900

    style : 검색 버튼 수정

--- src/components/atoms/SearchButton.tsx
+++ src/components/atoms/SearchButton.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const SearchButton = () => {
   return (
     <Link href="/search" className="flex items-center cursor-pointer">
-      <img src="/search.png" alt="돋보기" className="w-8 h-8" />
+      <img src="/search.png" alt="돋보기" className="mr-[3px]" />
     </Link>
   );
 };
