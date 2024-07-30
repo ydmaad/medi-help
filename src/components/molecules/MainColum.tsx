452eb6602743d5be489f1be2145ccb654f2b248e
lsy8181 <a4630486@gmail.com>
2024-07-30 17:31:58 +0900

    style : 메인칼럼, 서브칼럼 생성

---
+++ src/components/molecules/MainColum.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ImageProvider } from "@/contexts/ImageContext";
+import MainImage from "../atoms/MainImage";
+import CardTitle from "../atoms/CardTitle";
+import { BarTextProvider } from "@/contexts/BarTextContext";
+import BarText from "../atoms/BarText";
+
+const MainColum = ({
+  src,
+  alt,
+  title,
+  leftText,
+  rightText,
+}: {
+  src: string;
+  alt: string;
+  title: string;
+  leftText: string;
+  rightText: string;
+}) => {
+  const barTextValue = {
+    leftText,
+    rightText,
+    setLeftText: (text: string) => {},
+    setRightText: (text: string) => {},
+  };
+
+  return (
+    <ImageProvider value={{ src, alt, title, subtitle: null }}>
+      <BarTextProvider value={barTextValue}>
+        <div className="mb-[56px]">
+          <MainImage />
+          <CardTitle />
+          <BarText />
+        </div>
+      </BarTextProvider>
+    </ImageProvider>
+  );
+};
+
+export default MainColum;
