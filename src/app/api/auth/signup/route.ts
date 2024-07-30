d7ae3630881116dcd86f3446eac1c05a86967f1e
sseul <hanbat107@naver.com>
2024-07-30 16:04:34 +0900

    fix : login, logout, signup 라우트 핸들러 주석처리

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,30 +1,32 @@
-import { NextRequest, NextResponse } from "next/server";
-import { supabase } from "@/utils/supabase/client";
+// ssr방식 미적용이라 라우트핸들러 주석처리
 
-export async function POST(request: NextRequest) {
-  try {
-    const { email, password } = await request.json();
+// import { NextRequest, NextResponse } from "next/server";
+// import { supabase } from "@/utils/supabase/client";
 
-    const { data: signUpData, error: signUpError } = await supabase.auth.signUp(
-      { email, password }
-    );
-    if (signUpError) {
-      return NextResponse.json({ error: signUpError.message }, { status: 500 });
-    }
+// export async function POST(request: NextRequest) {
+//   try {
+//     const { email, password } = await request.json();
 
-    const { error: insertError } = await supabase.from("users").insert([
-      {
-        id: signUpData.user?.id,
-        email,
-        avatar: "",
-      },
-    ]);
-    if (insertError) {
-      return NextResponse.json({ error: insertError.message }, { status: 500 });
-    }
+//     const { data: signUpData, error: signUpError } = await supabase.auth.signUp(
+//       { email, password }
+//     );
+//     if (signUpError) {
+//       return NextResponse.json({ error: signUpError.message }, { status: 500 });
+//     }
 
-    return NextResponse.json({ message: "회원가입이 완료되었습니다." });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+//     const { error: insertError } = await supabase.from("users").insert([
+//       {
+//         id: signUpData.user?.id,
+//         email,
+//         avatar: "",
+//       },
+//     ]);
+//     if (insertError) {
+//       return NextResponse.json({ error: insertError.message }, { status: 500 });
+//     }
+
+//     return NextResponse.json({ message: "회원가입이 완료되었습니다." });
+//   } catch (error: any) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
