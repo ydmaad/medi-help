c5689860d819a3c5a743c04ee6f7dab17271db61
DO YEON KIM <rla1eh2dus3@naver.com>
2024-07-31 20:31:11 +0900

    fix : 마이페이지 복약기록 세팅

--- src/components/templates/mypage/Commnets.tsx
+++ src/components/templates/mypage/Commnets.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Comments() {
-  return <div>사용자가 작성한 댓글 구역</div>;
+  return <div>사용자가 작성한 게시글 구역</div>;
 }
---
+++ src/components/templates/mypage/MediLists.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Details() {
+  return <div>유저 복용약 구역</div>;
+}
