77b228e02ce047173777e67eb71f9f565eff5f43
Jiyoung <jiyoung990602@gmail.com>
2024-08-01 21:27:13 +0900

    fix : conflict 해결

--- src/app/(root)/mypage/page.tsx
+++ src/app/(root)/mypage/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Details from "@/components/templates/mypage/Details";
 import MediLists from "@/components/templates/mypage/MediLists";
 import Posts from "@/components/templates/mypage/posts";
+import UserBoard from "@/components/templates/mypage/UserBoard";
 
 const MyPage: React.FC = () => {
   return (
