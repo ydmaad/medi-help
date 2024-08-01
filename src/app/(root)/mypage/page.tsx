4022d798a4bd42807b2bd541ed599b4b07624c79
Jiyoung <jiyoung990602@gmail.com>
2024-08-01 21:22:10 +0900

    add : mypage user 정보 setting

--- src/app/(root)/mypage/page.tsx
+++ src/app/(root)/mypage/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Details from "@/components/templates/mypage/Details";
 import Comments from "@/components/templates/mypage/Commnets";
+import UserBoard from "@/components/templates/mypage/UserBoard";
 
 const MyPage: React.FC = () => {
   return (
     <div>
       <h1>My Page</h1>
+      <UserBoard />
       <Details />
       <Comments />
     </div>
