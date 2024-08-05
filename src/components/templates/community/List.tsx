0bc1883073c18e820063d83231c9f931ee26ecbf
ydmaad <ydmaad@gmail.com>
2024-08-05 15:25:50 +0900

    update: 게시글 리스트 스켈레톤 추가

--- src/components/templates/community/List.tsx
+++ src/components/templates/community/List.tsx
@@ -3,7 +3,7 @@
 import { Tables } from "@/types/supabase";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 type Post = Tables<"posts">;
 type User = Tables<"users">;
@@ -22,7 +22,26 @@ const fetchPosts = async () => {
   return data;
 };
 
+// 게시글 리스트 스켈레톤
+const ListSkeleton = () => {
+  return (
+    <li className="border p-4 w-[1000px] h-[150px] my-5 animate-pulse">
+      <div className="flex justify-between">
+        <div className="flex-grow pr-4">
+          <div className="h-7 bg-gray-200 rounded w-3/4 mb-2"></div>
+          <div className="h-4 bg-gray-200 rounded w-full mb-1"></div>
+          <div className="h-4 bg-gray-200 rounded w-2/3 mb-6"></div>
+          <div className="h-4 bg-gray-200 rounded w-1/5 mt-2"></div>
+        </div>
+        <div className="w-24 h-24 bg-gray-200 rounded flex-shrink-0"></div>
+      </div>
+    </li>
+  );
+};
+
 const List: React.FC<ListProps> = ({ searchTerm, posts, setPosts }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       // console.log("검색어가 업데이트 돼는 부분!?!?", searchTerm);
@@ -31,6 +50,8 @@ const List: React.FC<ListProps> = ({ searchTerm, posts, setPosts }) => {
         setPosts(response.data);
       } catch (error) {
         console.log("에러가 났네요 =>", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -55,6 +76,17 @@ const List: React.FC<ListProps> = ({ searchTerm, posts, setPosts }) => {
   const getImageUrls = (urlString: string | null): string[] => {
     return urlString ? urlString.split(",").map((url) => url.trim()) : [];
   };
+
+  // 게시글 로딩중 스켈레톤 적용
+  if (isLoading) {
+    return (
+      <ul className="space-y-4">
+        {[...Array(6)].map((_, index) => (
+          <ListSkeleton key={index} />
+        ))}
+      </ul>
+    );
+  }
 
   return (
     <>
