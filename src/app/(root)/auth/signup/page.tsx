3dea0a2334896967d20d7683c7cf55e9dc145a06
sseul <hanbat107@naver.com>
2024-08-09 04:16:42 +0900

    update : 카카오 로그인 연동완료. 네비바 닉네임 받아오기 성공

--- src/app/(root)/auth/signup/page.tsx
+++ src/app/(root)/auth/signup/page.tsx
@@ -38,21 +38,26 @@ export default function SignupPage() {
       const { data, error } = await supabase.auth.signUp({
         email,
         password,
+        options: {
+          data: {
+            name: nickname,
+          },
+        },
       });
 
       if (error) throw error;
 
       if (data.user) {
         // 사용자 추가 정보를 데이터베이스에 저장
-        const { error: insertError } = await supabase.from("users").insert([
-          {
-            id: data.user.id,
-            email,
-            nickname,
-          },
-        ]);
+        // const { error: insertError } = await supabase.from("users").insert([
+        //   {
+        //     id: data.user.id,
+        //     email,
+        //     nickname,
+        //   },
+        // ]);
 
-        if (insertError) throw insertError;
+        // if (insertError) throw insertError;
 
         // 저장된 사용자 정보 조회
         const { data: userData, error: fetchError } = await supabase
