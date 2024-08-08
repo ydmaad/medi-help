e525bd83042073e5d44c17dd1b3e801ae1e434b7
Jiyoung <jiyoung990602@gmail.com>
2024-08-09 05:05:22 +0900

    fix : css 수정

--- src/app/(root)/calendar/page.tsx
+++ src/app/(root)/calendar/page.tsx
@@ -7,9 +7,9 @@ import React from "react";
 const Page = () => {
   return (
     <div className="flex min-h-screen">
-      {/* <div className="w-1/4 p-4 bg-gray-100 overflow-y-auto">
+      <div className="max-[414px]:hidden w-1/4 p-4 bg-gray-100 overflow-y-auto">
         <MediRecords />
-      </div> */}
+      </div>
       <div className="w-3/4">
         <CalendarView />
       </div>
