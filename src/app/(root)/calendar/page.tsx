14a8fc7d66cb4534346c76c93fd5a0a746622900
DO YEON KIM <rla1eh2dus3@naver.com>
2024-08-11 23:27:34 +0900

    fix : test

--- src/app/(root)/calendar/page.tsx
+++ src/app/(root)/calendar/page.tsx
@@ -10,11 +10,10 @@ const Page = () => {
     <div className="flex w-full min-h-screen">
       <div className="max-[414px]:hidden min-w-[240px] min-h-screen p-4 bg-gray-100 overflow-y-auto">
         <MediRecords />
-        <CalendarCheckbox />
+        <CalendarCheckbox /> 
       </div>
       <div>
         <CalendarView />
-        
       </div>
     </div>
   );
