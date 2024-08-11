c60af6b35b7fa5706f956b68f121a81d18c4866a
ydmaad <ydmaad@gmail.com>
2024-08-11 23:30:25 +0900

    update: 댓글 추가시 따닥(중복등록)방지 추가

--- src/components/templates/community/Comments.tsx
+++ src/components/templates/community/Comments.tsx
@@ -3,7 +3,7 @@
 import { useAuthStore } from "@/store/auth";
 import { Tables } from "@/types/supabase";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 interface CommentsProps {
   postId: string;
@@ -14,6 +14,9 @@ type User = Tables<"users">;
 type CommentWithUser = Comment & {
   user: Pick<User, "avatar" | "nickname" | "id">;
 };
+
+// 학습 필요!!
+type FunctionType<Args extends unknown[], Return> = (...args: Args) => Return;
 
 // 댓글 가져오기 요청
 const fetchComment = async (postId: string) => {
@@ -94,6 +97,35 @@ const postComment = async (postId: string, comment: string) => {
   }
 };
 
+// 댓글 따닥 방지 훅(useThrottle이라는 커스텀 훅) - 실행할 함수(callback)와 쓰로틀링간격(delay)를 인수로 받음
+function useThrottle<Args extends unknown[], Return>(
+  callback: FunctionType<Args, Return>,
+  delay: number // 밀리초 단위의 시간 간격
+): FunctionType<Args, Return> {
+  // 마지막으로 함수가 실행된 시간을 저장
+  const lastRun = useRef(Date.now());
+  // 메모이제이션된 콜백을 반환 - 불필요한 렌더링 방지
+  return useCallback(
+    // 나중에 이 함수가 호출될 때 전달될 모든 인자를 받아들임
+    (...args: Args): Return => {
+      // 현재 시간을 가져옴
+      const now = Date.now();
+      // 현재 시간과 마지막 실행된 시간을 뺐을 때 delay(2초)보다 크거나 같다면
+      if (now - lastRun.current >= delay) {
+        // 마지막 실행시간을 현재시간으로 업데이트
+        lastRun.current = now;
+        // 원본 콜백함수를 실행하고 그 결과를 반환
+        return callback(...args);
+      }
+      // 타입 안전성을 위해 실제 반환값의 타입을 사용
+      // 이 부분은 실행되지 않지만, TypeScript의 타입 체크를 통과하기 위해 필요
+      return undefined as unknown as Return;
+    },
+    // callback이나 delay가 변경될 때만 새로운 함수를 생성
+    [callback, delay]
+  );
+}
+
 const Comments = ({ postId }: CommentsProps) => {
   const [comment, setComment] = useState<CommentWithUser[]>([]);
   const [newComment, setNewComment] = useState("");
@@ -103,6 +135,7 @@ const Comments = ({ postId }: CommentsProps) => {
   );
   const { user } = useAuthStore();
 
+  // 댓글 불러오는 중
   useEffect(() => {
     const getComment = async () => {
       try {
@@ -142,7 +175,7 @@ const Comments = ({ postId }: CommentsProps) => {
   };
 
   // 댓글 달기 핸들러
-  const handleAddComment = async () => {
+  const handleAddComment = useThrottle(async () => {
     if (!newComment.trim()) {
       alert("댓글을 입력해주세요.");
       return;
@@ -157,7 +190,7 @@ const Comments = ({ postId }: CommentsProps) => {
       console.error("댓글 추가 실패 :", (error as Error).message);
       alert("댓글 추가에 실패했습니다. 다시 시도해 주세요.");
     }
-  };
+  }, 2000);
 
   // 수정 모드 전환 핸들러
   const handleEdit = (
