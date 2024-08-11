391555ac533dd620faf57cfdc49edb8254693657
ydmaad <ydmaad@gmail.com>
2024-08-12 01:46:20 +0900

    fix: 커뮤니티 검색결과 없음 ui 수정, searchFail.svg 이미지 추가

--- src/components/templates/community/List.tsx
+++ src/components/templates/community/List.tsx
@@ -269,12 +269,27 @@ const List = ({ searchTerm, posts, setPosts }: ListProps) => {
             );
           })
         ) : (
-          <div className="flex flex-col items-center justify-center h-[400px] text-center">
-            <p className="text-2xl font-semibold text-gray-600 mb-4">
-              <span className="text-brand-primary-500">{searchTerm}</span> 의
-              대한 검색 결과가 없습니다
+          <div>
+            <p className="text-brand-gray-1000 font-black text-xl mt-20">
+              <span className="text-brand-primary-500">"{searchTerm}"</span> 에
+              대한 검색 결과{" "}
+              <span className="text-brand-gray-600">
+                ({filteredPosts.length})
+              </span>
             </p>
-            <p className="text-gray-500">다른 키워드로 검색해 주세요.</p>
+
+            <div className="flex items-center justify-center h-[400px] text-center">
+              <Image
+                src={"/searchFail.svg"}
+                alt="검색결과 없음 이미지"
+                width={70}
+                height={104}
+                className="mx-4"
+              ></Image>
+              <p className="text-2xl font-semibold text-brand-gray-600 mb-4">
+                해당 검색에 대한 결과를 찾을 수 없어요.
+              </p>
+            </div>
           </div>
         )}
       </div>
