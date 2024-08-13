49ead133e28c308dfecdab3dbd64b60cf314d0b4
sseul <hanbat107@naver.com>
2024-08-13 19:07:38 +0900

    refactor : AuthErrorMessage 불필요한 코드 정리

--- src/components/atoms/AuthErrorMessage.tsx
+++ src/components/atoms/AuthErrorMessage.tsx
@@ -1,13 +1,4 @@
 // 목적: 인증 과정에서 발생하는 오류 메시지를 표시하는 컴포넌트
-// import React from "react";
-
-// type AuthErrorMessageProps = {
-//   message: string;
-// };
-
-// export const AuthErrorMessage: React.FC<AuthErrorMessageProps> = ({
-//   message,
-// }) => <p className="text-red-500 text-sm">{message}</p>;
 
 import React from "react";
 
