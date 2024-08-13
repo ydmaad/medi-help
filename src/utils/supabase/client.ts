5b06e55ead12ef9bddcd1a95eff16e52f502b558
sseul <hanbat107@naver.com>
2024-08-13 19:17:09 +0900

    refactor : client 불필요한 코드 정리

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -4,24 +4,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
-
-// 최신 코드
-// src/utils/supabase/client.ts
-
-// import { createBrowserClient } from "@supabase/ssr";
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-// export const supabase = createBrowserClient(supabaseUrl!, supabaseAnonKey!);
-
-// 기존 코드
-// import { Database } from "@/types/supabase";
-// import { createClient } from "@supabase/supabase-js";
-
-// export const supabase = createClient<Database>(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-// );
-
-// src/utils/supabase/client.ts
