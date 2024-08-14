073e12436f80f35e0ac43da80d9b2dacc5bdc2fe
DO YEON KIM <rla1eh2dus3@naver.com>
2024-08-14 18:09:42 +0900

    style : 마이페이지 웹 ui 수정

--- src/components/templates/mypage/UserBoard.tsx
+++ src/components/templates/mypage/UserBoard.tsx
@@ -15,24 +15,23 @@ const UserBoard: React.FC<UserBoardProps> = ({ className }) => {
 
   return (
     <div
-      className={`flex flex-col items-center w-full p-12 bg-[#e9f5fe] rounded-xl text-primary-500 justify-center ${className}`}
-      style={{ height: '100%' }} // Set to 100% to fill parent container
+      className={`flex flex-col items-center w-full p-6 bg-[#e9f5fe] rounded-xl text-primary-500 ${className}`}
     >
-      <div className="relative w-60 h-60 rounded-full overflow-hidden mb-8">
+      <div className="w-24 h-24 desktop:w-60 desktop:h-60 rounded-full overflow-hidden mb-4">
         <img
           src={user?.avatar ? user.avatar : "/default-avatar.jpg"}
           alt="프로필 이미지"
           className="w-full h-full object-cover rounded-full"
         />
       </div>
-      <div className="flex flex-col items-center">
-        <div className="text-4xl font-bold text-primary-500 mb-2">
+      <div className="flex flex-col items-center w-full">
+        <div className="text-2xl desktop:text-4xl font-bold text-primary-500 mb-2">
           {user?.nickname}님
         </div>
-        <div className="text-xl text-gray-800 mb-6">
+        <div className="text-lg desktop:text-xl text-gray-800 mb-6">
           {user?.email}
         </div>
-        <button className="w-full min-w-40 py-3 flex items-center justify-center gap-2 bg-[#279ef9] text-[#f5f6f7] rounded-full cursor-pointer hover:bg-[#1e7fe0] ease-in duration-300">
+        <button className="w-full max-w-[240px] py-3 flex items-center justify-center gap-2 bg-[#279ef9] text-[#f5f6f7] rounded-xl cursor-pointer hover:bg-[#1e7fe0] ease-in duration-300">
           <TbPencil className="text-2xl" />
           프로필 수정
         </button>
