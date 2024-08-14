95d9d155c4a97e4403c163dc660ff6bf70e46147
ydmaad <ydmaad@gmail.com>
2024-08-14 16:02:33 +0900

    fix:오타수정

--- src/components/templates/community/Search.tsx
+++ src/components/templates/community/Search.tsx
@@ -22,7 +22,7 @@ const Search = ({ handleSearch }: SearchProps) => {
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="제목 및 내용, 작성자 등을 검색해세요"
+            placeholder="제목 및 내용, 작성자 등을 검색하세요"
             className="w-[300px] border-solid border-2 border-brand-primary-300 py-2 pl-10 pr-4 text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
