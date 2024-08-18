fd1896d6dcc0c082c7d6163282299f37a534d51f
sseul <hanbat107@naver.com>
2024-08-19 01:36:28 +0900

    refactor : AuthButton 불필요한 코드 정리

--- src/components/atoms/AuthButton.tsx
+++ src/components/atoms/AuthButton.tsx
@@ -29,32 +29,3 @@ export const AuthButton: React.FC<AuthButtonProps> = ({
     {children}
   </button>
 );
-
-// import React from "react";
-
-// type AuthButtonProps = {
-//   children: React.ReactNode;
-//   onClick?: () => void;
-//   type?: "button" | "submit" | "reset";
-//   className?: string;
-//   disabled?: boolean;
-// };
-
-// export const AuthButton: React.FC<AuthButtonProps> = ({
-//   children,
-//   onClick,
-//   type = "button",
-//   className = "",
-//   disabled = false,
-// }) => (
-//   <button
-//     type={type}
-//     onClick={onClick}
-//     className={`w-full h-[48px] text-[18px] bg-brand-primary-500 text-white font-semibold py-[8px] rounded ${className} ${
-//       disabled ? "opacity-50 cursor-not-allowed" : ""
-//     }`}
-//     disabled={disabled}
-//   >
-//     {children}
-//   </button>
-// );
