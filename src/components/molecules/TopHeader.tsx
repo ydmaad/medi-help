70ab032b4255c013df5d0644d83b58f82818a10e
lsy8181 <a4630486@gmail.com>
2024-08-19 01:00:46 +0900

    update:테탑때는 안보이도록 수정

--- src/components/molecules/TopHeader.tsx
+++ src/components/molecules/TopHeader.tsx
@@ -66,7 +66,7 @@ const Header = () => {
         </div>
         <LoginNav />
       </header>
-      <div className="absolute z-20">
+      <div className="absolute z-20 desktop:hidden">
         <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
       </div>
     </>
