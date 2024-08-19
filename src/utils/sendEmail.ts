d439dd7c75cf09548219a6df5eef3fa8757af919
DO YEON KIM <rla1eh2dus3@naver.com>
2024-08-19 20:32:16 +0900

    fix: 알람 바로오는 문제 수정

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -28,7 +28,10 @@ export const sendEmail = async (options: MailOptions) => {
     html,
   };
 
-  console.log('Attempting to send email with options:', mailOptions);
+  console.log('Attempting to send email with options:', {
+    ...mailOptions,
+    text: mailOptions.text.substring(0, 100) + '...',  // 긴 텍스트는 일부만 로그에 출력
+  });
 
   try {
     const info = await transporter.sendMail(mailOptions);
@@ -40,18 +43,20 @@ export const sendEmail = async (options: MailOptions) => {
   }
 };
 
+// 이 함수는 예시용이므로 주석 처리하거나 필요 없다면 제거할 수 있습니다.
+// const exampleUsage = async (newMediRecord: any, user: any) => {
+//   const { subject, message } = generateNotificationMessage({
+//     medi_nickname: newMediRecord.medi_nickname,
+//     medi_name: newMediRecord.medi_name,
+//     user_nickname: user.data.nickname,
+//     notes: newMediRecord.notes,
+//   });
 
-const exampleUsage = async (newMediRecord: any, user: any) => {
-  const { subject, message } = generateNotificationMessage({
-    medi_nickname: newMediRecord.medi_nickname,
-    medi_name: newMediRecord.medi_name,
-    user_nickname: user.data.nickname,
-    notes: newMediRecord.notes,
-  });
+//   await sendEmail({
+//     to: user.data.email,
+//     subject,
+//     text: message,
+//   });
+// };
 
-  await sendEmail({
-    to: user.data.email,
-    subject,
-    text: message,
-  });
-};
+export default sendEmail;
