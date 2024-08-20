5ffac4e7b233ba8fba88e0bc5c6896a8fb8a7630
lsy8181 <a4630486@gmail.com>
2024-08-20 14:35:01 +0900

    update: 상단바 수정

--- src/app/(root)/community/[id]/page.tsx
+++ src/app/(root)/community/[id]/page.tsx
@@ -1,9 +1,13 @@
 import PostDetail from "@/components/templates/community/PostDetail";
+import Header from "@/components/molecules/TopHeader";
 
 const PostDetailPage = ({ params }: { params: { id: string } }) => {
   return (
     <>
       <div className="mt-[114px] desktop:mt-[160px]">
+        <div className="desktop:hidden">
+          <Header showBackHeader={true} />
+        </div>
         <PostDetail id={params.id} />
       </div>
     </>
