a60954ae468d46d9b09bb8196ae2c85ce2516fe2
ydmaad <ydmaad@gmail.com>
2024-08-20 19:21:58 +0900

    style:댓글 ui 수정

--- src/components/molecules/CommentInput.tsx
+++ src/components/molecules/CommentInput.tsx
@@ -47,20 +47,20 @@ export const CommentInput = ({
 
   return (
     <>
-      <div className="max-w-[996px] p-4 bg-white  border border-gray-300  rounded-lg">
-        <div className="flex items-center mb-3">
+      <div className="max-w-[996px] h-[162px] p-[15px] flex flex-col bg-white  border border-gray-300  rounded-lg">
+        <div className="flex items-center mb-0 desktop:mb-3">
           <Image
             src={user?.avatar || "/defaultAvatar.svg"}
             alt={"유저 이미지"}
-            width={40}
-            height={40}
+            width={24}
+            height={24}
             className="rounded-full mr-3 aspect-square object-cover"
           />
-          <h2 className="text-l mt-[15px] mb-4">{user?.nickname}</h2>
+          <h2 className="text-[14px] desktop:text-[16px] ">{user?.nickname}</h2>
         </div>
 
         <textarea
-          className="w-full text-xs px-2  focus:outline-none  resize-none"
+          className="w-full text-[14px] px-2 mt-[15px] desktop:mt-[10px] focus:outline-none  resize-none flex-grow"
           placeholder={`댓글을 입력해주세요.\n게시글과 무관한 악성 댓글은 삭제될 수 있습니다.`}
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
@@ -69,7 +69,7 @@ export const CommentInput = ({
         <div className="flex justify-end">
           <button
             onClick={handleAddComment}
-            className={`w-[90px]   px-4 py-2 rounded-lg  focus:outline-none focus:ring-2 focus:ring-brand-primary-600 ${newComment.length > 0 ? "bg-brand-primary-500 text-white hover:bg-brand-primary-600" : "bg-brand-gray-200 text-brand-gray-600"}`}
+            className={`w-[72px] desktop:w-[90px] h-[32px] desktop:h-[40px] text-[14px] desktop:text-[16px]px-4 rounded-lg  focus:outline-none focus:ring-2 focus:ring-brand-primary-600 ${newComment.length > 0 ? "bg-brand-primary-500 text-white hover:bg-brand-primary-600" : "bg-brand-gray-200 text-brand-gray-600"}`}
           >
             등록
           </button>
