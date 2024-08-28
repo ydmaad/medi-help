a91756561ef61a66facec8299ac22f8c04dd310a
sseul <hanbat107@naver.com>
2024-08-29 02:01:53 +0900

    style : 비밀번호 눈 아이콘 패딩값 조절

--- src/components/molecules/AuthPasswordInput.tsx
+++ src/components/molecules/AuthPasswordInput.tsx
@@ -40,7 +40,7 @@ export const AuthPasswordInput: React.FC<AuthPasswordInputProps> = ({
       <button
         type="button"
         onClick={() => setShowPassword(!showPassword)}
-        className="absolute inset-y-0 right-0 pr-3 flex items-center"
+        className="absolute inset-y-0 right-0 pl-3 pr-3 flex items-center"
       >
         <Image
           src={showPassword ? "/close_eye_icon.svg" : "/open_eye_icon.svg"}
