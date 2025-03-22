a5feba6276078dbfa69eb6567f69189e374f80b6
Jiyoung <jiyoung990602@gmail.com>
2025-03-22 12:28:19 +0900

    mediNames 에의존하지 않는 api 호출 로직 분리

--- src/components/templates/calendar/CalendarTemplate.tsx
+++ src/components/templates/calendar/CalendarTemplate.tsx
@@ -35,107 +35,126 @@ const CalendarTemplate = () => {
     if (user) {
       setValues({ ...values, user_id: user.id });
 
-      getEventsData();
+      const getCalendarData = async () => {
+        if (user) {
+          try {
+            const { data } = await axios.get(
+              `/api/calendar?user_id=${user.id}`
+            );
+
+            // 복약기록 날짜 및 노트 정보
+            const newCalendar: CalendarType[] = [];
+            data.map((info: CalendarType) => {
+              newCalendar.push({
+                id: info.id,
+                user_id: info.user_id,
+                created_at: info.created_at,
+                side_effect: info.side_effect,
+                start_date: info.start_date,
+              });
+            });
+
+            setCalendar(newCalendar);
+          } catch (error) {
+            console.log("axios error", error);
+          }
+        }
+      };
+
+      // 복용중인 약 불러오는 로직
+      const getMedicines = async () => {
+        if (user) {
+          try {
+            const { data } = await axios.get(
+              `/api/calendar/medi?user_id=${user.id}`
+            );
+
+            const newMedicines: MedicinesType[] = [];
+
+            data.medicationRecords.map((record: any) => {
+              newMedicines.push({
+                id: record.id,
+                name: record.medi_nickname,
+                time: record.times,
+                notification_time: record.notification_time,
+              });
+            });
+
+            setMedicines(newMedicines);
+            return data;
+          } catch (error) {
+            console.log("medi axios =>", error);
+          }
+        }
+      };
+
+      getCalendarData();
       getMedicines();
     }
-  }, [user, mediNames]);
+  }, [user]);
 
-  const getEventsData = async () => {
-    try {
+  useEffect(() => {
+    const getEventsData = async () => {
       if (user) {
-        const { data } = await axios.get(`/api/calendar?user_id=${user.id}`);
+        try {
+          const { data } = await axios.get(`/api/calendar?user_id=${user.id}`);
 
-        {
-          const newEvents: EventInput[] = [];
-          data.map((event: EventInput) => {
-            if (event.calendar_medicine.length !== 0) {
-              const setEventList = (time: string) => {
-                let eventList = event.calendar_medicine.filter(
-                  (medicine: any) => {
-                    return medicine.medi_time === time;
-                  }
-                );
+          {
+            const newEvents: EventInput[] = [];
+            data.map((event: EventInput) => {
+              if (event.calendar_medicine.length !== 0) {
+                const setEventList = (time: string) => {
+                  let eventList = event.calendar_medicine.filter(
+                    (medicine: any) => {
+                      return medicine.medi_time === time;
+                    }
+                  );
 
-                const newEventList = eventList.filter((e: any) => {
-                  return mediNames.includes(e.medications.medi_nickname);
-                });
-
-                let countMedicines = newEventList.length;
-
-                if (countMedicines !== 0) {
-                  let medicineNickname =
-                    newEventList[0].medications.medi_nickname;
-                  newEvents.push({
-                    groupId: event.id,
-                    title:
-                      countMedicines !== 1
-                        ? `${medicineNickname} 외 ${countMedicines - 1}개`
-                        : `${medicineNickname}`,
-                    start: `${event.start_date} ${
-                      TIME_OF_TIME[newEventList[0].medi_time]
-                    }`,
-                    extendProps: {
-                      medi_time: newEventList[0].medi_time,
-                      medicineList: newEventList.map(
-                        (medicine: any) => medicine.medications.id
-                      ),
-                    },
+                  const newEventList = eventList.filter((e: any) => {
+                    return mediNames.includes(e.medications.medi_nickname);
                   });
-                }
-              };
 
-              setEventList("morning");
-              setEventList("afternoon");
-              setEventList("evening");
-            }
-          });
-          setEvents(newEvents);
+                  let countMedicines = newEventList.length;
+
+                  if (countMedicines !== 0) {
+                    let medicineNickname =
+                      newEventList[0].medications.medi_nickname;
+                    newEvents.push({
+                      groupId: event.id,
+                      title:
+                        countMedicines !== 1
+                          ? `${medicineNickname} 외 ${countMedicines - 1}개`
+                          : `${medicineNickname}`,
+                      start: `${event.start_date} ${
+                        TIME_OF_TIME[newEventList[0].medi_time]
+                      }`,
+                      extendProps: {
+                        medi_time: newEventList[0].medi_time,
+                        medicineList: newEventList.map(
+                          (medicine: any) => medicine.medications.id
+                        ),
+                      },
+                    });
+                  }
+                };
+
+                setEventList("morning");
+                setEventList("afternoon");
+                setEventList("evening");
+              }
+            });
+            setEvents(newEvents);
+          }
+
+          setIsLoading(false);
+        } catch (error) {
+          console.log("axios error", error);
         }
-        setIsLoading(false);
-
-        const newCalendar: CalendarType[] = [];
-        data.map((info: CalendarType) => {
-          newCalendar.push({
-            id: info.id,
-            user_id: info.user_id,
-            created_at: info.created_at,
-            side_effect: info.side_effect,
-            start_date: info.start_date,
-          });
-        });
-        setCalendar(newCalendar);
       }
-    } catch (error) {
-      console.log("axios error", error);
-    }
-  };
+    };
 
-  // 복용중인 약 불러오는 로직
-  const getMedicines = async () => {
-    try {
-      if (user) {
-        const { data } = await axios.get(
-          `/api/calendar/medi?user_id=${user.id}`
-        );
-
-        const newMedicines: MedicinesType[] = [];
-
-        data.medicationRecords.map((record: any) => {
-          newMedicines.push({
-            id: record.id,
-            name: record.medi_nickname,
-            time: record.times,
-            notification_time: record.notification_time,
-          });
-        });
-
-        setMedicines(newMedicines);
-        return data;
-      }
-    } catch (error) {
-      console.log("medi axios =>", error);
-    }
-  };
+    getEventsData();
+  }, [user, mediNames]);
 
   if (isLoading) {
     return (
